7b35b0225d4f67c10242502a13ce3a144417ffc3
RK1547 <141389818+RK1547@users.noreply.github.com>
2024-06-06 17:33:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const display = document.getElementById('display');
+
+      function clearDisplay() {
+        display.value = '';
+      }
+
+      function deleteLast() {
+        display.value = display.value.slice(0, -1);
+      }
+
+      function appendToDisplay(value) {
+        display.value += value;
+      }
+
+      function calculate() {
+        try {
+          display.value = eval(display.value);
+        } catch (e) {
+          display.value = 'Error';
+        }
+      }
